20524a0d1ac655888cf396f730fc4961c85a4191
Bogdan Bardakov <bogdan_bardakov@epam.com>
2017-05-05 23:01:01 +0300

    leaks with non-closed br and scanner and intern

--- src/main/java/MemoryLeaksApp.java
+++ src/main/java/MemoryLeaksApp.java
@@ -1,18 +1,30 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class MemoryLeaksApp {
 
+    private static final String FILE_NAME = "longFile.txt";
+
     public static void main(String[] args) {
         MemoryLeaksApp memoryLeaksApp = new MemoryLeaksApp();
-        memoryLeaksApp.getLongFile();
+        //push string in memory pool
+        memoryLeaksApp.getLongFile().intern();
     }
 
     private String getLongFile() {
         try {
             ClassLoader classLoader = getClass().getClassLoader();
-            BufferedReader br = new BufferedReader(new FileReader(classLoader.getResource("longText.txt").getFile()));
-            String str = br.readLine();
+            //not closed BR
+            BufferedReader br = new BufferedReader(new FileReader(classLoader.getResource(FILE_NAME).getFile()));
+            //not closed scanner
+            Scanner scanner = new Scanner(br);
+            String str = FILE_NAME;
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                str = str + line;
+            }
             return str;
         } catch (Exception e) {
             e.printStackTrace();
